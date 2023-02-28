ae859758868151eb7a89fb544e438b146e90cc4d
Kelvin Luk <K_luk2@u.pacific.edu>
2023-02-27 22:15:17 -0800

    Testing main.py

--- MedicalAI/Pycharm Folder/main.py
+++ MedicalAI/Pycharm Folder/main.py
@@ -1,6 +1,6 @@
 # This is a sample Python script.
 # Mansoor Haidari
-
+# Kelvin    Luk
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
