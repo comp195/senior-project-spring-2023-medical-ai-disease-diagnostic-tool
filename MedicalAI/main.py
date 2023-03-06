3cb3f69852fcab1fd5a5d71303f00a1047fad8aa
Kelvin Luk <K_luk2@u.pacific.edu>
2023-03-05 17:10:16 -0800

    File Io  / read excel data

--- MedicalAI/main.py
+++ MedicalAI/main.py
@@ -15,3 +15,24 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Read File Io---------------------------------------------------------------------------------------------
+with open('cardio_training10Set.csv', 'r') as f:
+    # skip the header line
+    next(f)
+    data = []
+    for line in f:
+        # split the line into a list of values
+        values = line.strip().split(';')
+        # convert the values to the appropriate data type
+        id_, age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active, cardio = (
+            int(values[0]), int(values[1]), int(values[2]), int(values[3]), float(values[4]), int(values[5]),
+            int(values[6]), int(values[7]), int(values[8]), int(values[9]), int(values[10]), int(values[11]),
+            int(values[12]))
+        # store the values in a nested list
+        data.append([id_, age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active, cardio])
+
+# print the first 5 rows of data
+    print("   id_, age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active, cardio")
+for row in data[:5]:
+    print(row)
