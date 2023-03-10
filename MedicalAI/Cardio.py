e03fc54ea0929899fb38a1aad5bac5c3bba84dfc
Kelvin Luk <K_luk2@u.pacific.edu>
2023-03-09 20:35:30 -0800

    Check Data and data input of heart training

--- MedicalAI/Cardio.py
+++ MedicalAI/Cardio.py
@@ -18,22 +18,22 @@ if __name__ == '__main__':
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 # Read File Io---------------------------------------------------------------------------------------------
-with open('../MedicalAI/Cardio_training10Set.csv', 'r') as f:
+with open('heart_training10Set.csv', 'r') as f:
     # skip the header line
     next(f)
     data = []
     for line in f:
         # split the line into a list of values
-        values = line.strip().split(';')
-        # convert the values to the appropriate data type
-        id_, age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active, cardio = (
-            int(values[0]), int(values[1]), int(values[2]), int(values[3]), float(values[4]), int(values[5]),
-            int(values[6]), int(values[7]), int(values[8]), int(values[9]), int(values[10]), int(values[11]),
-            int(values[12]))
-        # store the values in a nested list
-        data.append([id_, age, gender, height, weight, ap_hi, ap_lo, cholesterol, gluc, smoke, alco, active, cardio])
+        values = line.strip().split(',')
 
-# print the first number of rows of data
-    print("   Age,Sex,ChestPainType,RestingBP,Cholesterol,FastingBS,RestingECG,MaxHR,ExerciseAngina,Oldpeak,ST_Slope,HeartDisease")
-for row in data[:10]:
-    print(row)
+        # convert the values to the appropriate data type
+        Age, Sex, ChestPainType, RestingBP, Cholesterol, FastingBS, RestingECG, MaxHR, ExerciseAngina, Oldpeak, ST_Slope, HeartDisease = (
+            int(values[0]), values[1], values[2], int(values[3]), int(values[4]), values[5],
+            values[6], int(values[7]), values[8], float(values[9]), values[10], int(values[11]))
+        # store the values in a nested list
+        data.append([Age, Sex, ChestPainType, RestingBP, Cholesterol, FastingBS, RestingECG, MaxHR, ExerciseAngina, Oldpeak, ST_Slope, HeartDisease])
+
+    # print the first number of rows of data
+    print("Age\tSex\tChestPainType\tRestingBP\tCholesterol\tFastingBS\tRestingECG\tMaxHR\tExerciseAngina\tOldpeak\tST_Slope\tHeartDisease")
+    for row in data[:10]:
+        print('\t'.join([str(val) for val in row]))
