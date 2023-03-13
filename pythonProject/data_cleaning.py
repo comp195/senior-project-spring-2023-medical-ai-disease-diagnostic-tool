c122c63e08c440f26c3f58b7d8c431a91594f116
mansoorhaidari <100883284+mansoorhaidari@users.noreply.github.com>
2023-03-12 17:31:01 -0700

    Cleaned python code and removed errors

--- pythonProject/data_cleaning.py
+++ pythonProject/data_cleaning.py
@@ -5,12 +5,6 @@ import csv
 
 # Open the CSV file for reading
 
-d = pd.read_csv('heart.csv')
-print(d.values)
-
-d.loc[:,"Age"]
-print(d.loc[:,"Age"])
-
 with open('heart.csv', 'r') as f:
 
     # Read lines from file
@@ -24,38 +18,43 @@ with open('heart.csv', 'r') as f:
 
 # Create a pandas DataFrame from the fields
 df = pd.read_csv('heart.csv')
-print(df)
+
 
 # Convert columns to numeric data types
 Age = df.loc[:,'Age']
-print(Age)
+
 
 RestingBP = df.loc[:,'RestingBP']
-print(RestingBP)
+
 
 Cholesterol = df.loc[:,'Cholesterol']
-print(Cholesterol)
+
 
 FastingBS = df.loc[:,'FastingBS']
-print(FastingBS)
+
 
 RestingECG = df.loc[:,'RestingECG']
-print(RestingECG)
+
 
 MaxHR= df.loc[:,'MaxHR']
-print(MaxHR)
+
 
 ExerciseAngina= df.loc[:,'ExerciseAngina']
-print(ExerciseAngina)
+
 
 Oldpeak = df.loc[:,'Oldpeak']
-print(Oldpeak)
+
 
 ST_Slope = df.loc[:,'ST_Slope']
-print(ST_Slope)
+
 
 HeartDisease = df.loc[:,'HeartDisease']
-print(HeartDisease)
+
+
+print(df)
+
+
+
 
 
 # Sort the DataFrame by the 'Age' column
@@ -66,13 +65,13 @@ low_value = sorted_df['Cholesterol'].min()
 high_value = sorted_df['Cholesterol'].max()
 
 # Identify the number of 'normal' and 'ST' values in the 'ST_Slope' column
-count_normal = sorted_df[sorted_df['ST_Slope'] == 1].shape[0]
-count_st = sorted_df[sorted_df['ST_Slope'] == 2].shape[0]
 
-# Loop through every value in the sorted DataFrame and print it
-for index, row in sorted_df.iterrows():
-    for col in sorted_df.columns:
-        print(row[col])
+count_normal = sorted_df['RestingECG'].value_counts()['Normal']
+count_st = sorted_df['RestingECG'].value_counts()['ST']
+count_lvh = sorted_df['RestingECG'].value_counts()['LVH']
+
+
+
 
 print('Lowest Cholesterol:', low_value)
 print('Highest Cholesterol:', high_value)
---
+++ pythonProject/prototype.py
@@ -0,0 +1,81 @@
+# Libraries we need for this project
+
+import inline as inline
+import matplotlib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas.core.frame
+import seaborn as sns
+
+import os
+
+print(os.listdir())
+
+import warnings
+
+warnings.filterwarnings('ignore')
+
+# our dataset from kaggle
+
+dataset = pd.read_csv("heart.csv")
+
+# our dataframe  and object in pandas
+
+type(dataset)
+pandas.core.frame.DataFrame
+
+# dataset Shape
+dataset.shape
+(500, 10)
+
+# Print some columns
+dataset.head(15)
+
+dataset.sample(5)
+
+dataset.describe()
+
+dataset.info()
+info = ["age", "1: male, 0: female",
+        "chest pain type, 1: typical angina, 2: atypical angina, 3: non-anginal pain, 4: asymptomatic",
+        "resting blood pressure", " serum cholesterol in mg/dl", "fasting blood sugar > 120 mg/dl",
+        "resting electrocardiograph results (values 0,1,2)", " maximum heart rate achieved",
+        "exercise induced angina", "oldpeak = ST depression induced by exercise relative to rest",
+        "the slope of the peak exercise ST segment", "number of major vessels (0-3) colored by fluoroscopy",
+        "thal: 3 = normal; 6 = fixed defect; 7 = reversible defect"]
+
+for i in range(len(info)):
+    print(dataset.columns[i] + ":\t\t\t" + info[i])
+
+    dataset["target"].describe()
+    dataset["target"].unique()
+    print(dataset.corr()["target"].abs().sort_values(ascending=False))
+    y = dataset["target"]
+
+    sns.countplot(y)
+
+    target_temp = dataset.target.value_counts()
+
+    print(target_temp)
+    print("Percentage of patience without heart problems: " + str(round(target_temp[0] * 100 / 303, 2)))
+    print("Percentage of patience with heart problems: " + str(round(target_temp[1] * 100 / 303, 2)))
+
+    # Alternatively,
+    # print("Percentage of patience with heart problems: "+str(y.where(y==1).count()*100/303))
+    # print("Percentage of patience with heart problems: "+str(y.where(y==0).count()*100/303))
+
+    # #Or,
+    # countNoDisease = len(df[df.target == 0])
+    # countHaveDisease = len(df[df.target == 1])
+    dataset["sex"].unique()
+    sns.barplot(dataset["sex"], y)
+    dataset["cp"].unique()
+    sns.barplot(dataset["cp"], y)
+    dataset["fbs"].describe()
+    dataset["fbs"].unique()
+    sns.barplot(dataset["fbs"], y)
+    dataset["restecg"].unique()
+    sns.barplot(dataset["restecg"], y)
+    dataset["exang"].unique()
+    sns.barplot(dataset["exang"], y)
