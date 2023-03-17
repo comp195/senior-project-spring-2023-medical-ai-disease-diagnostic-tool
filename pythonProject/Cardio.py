9f70b596e570c0a2020f776a0b83fb6ea675706c
Kngk0 <k_westbrook@u.pacific.edu>
2023-03-16 19:32:47 -0700

    loaded dataset and printed information for preprocessing

--- pythonProject/Cardio.py
+++ pythonProject/Cardio.py
@@ -3,14 +3,16 @@ import pandas as pd
 import sklearn
 import matplotlib.pyplot as plt
 import seaborn as sns
-#import tensorflow as tf
-#import keras
+# import tensorflow as tf
+# import keras
 import torch
 import os
 
-print(os.getcwd())
+print(os.getcwd(), "\n")
 
 # load the dataset
 data = pd.read_csv('heart.csv', delimiter=',')
-print(data.head())
-
+print("Head:\n", data.head(), "\n") #display the first few rows of the DataFrame
+print("Shape:\n", data.shape, "\n") #returns the dimensions of the DataFrame as a tuple
+print("Info:\n", data.info(), "\n") #provides information about the DataFrame
+print("Describe:\n", data.describe(), "\n") #provides descriptive statistics about each column in the DataFrame
