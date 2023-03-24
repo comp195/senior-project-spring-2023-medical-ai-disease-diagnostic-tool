e9b7eeee8296ee131cf720e34432f8c4b106acd4
Kngk0 <k_westbrook@u.pacific.edu>
2023-03-23 19:36:40 -0700

    fixed file so it can return the actual dataframe

--- pythonProject/data_loader.py
+++ pythonProject/data_loader.py
@@ -16,8 +16,10 @@ def data_loader():
     data = pd.read_csv('heart.csv', delimiter=',')
     info_str = ""
     info_str += "Head:\n" + str(data.head(5)) + "\n"  # display the first few rows of the DataFrame
-    info_str += "Shape:\n" + str(data.shape) + "\n"  # returns the dimensions (rows, columns) of the DataFrame as a tuple
+    info_str += "Shape:\n" + str(data.shape) + "\n"  # returns the dimensions (rows, columns) of the DataFrame as a
+    # tuple
     info_str += str(data.info()) + "\n"  # provides information about the DataFrame
-    info_str += "Describe:\n" + str(data.describe()) + "\n"  # provides descriptive statistics about each column in the DataFrame
+    info_str += "Describe:\n" + str(data.describe()) + "\n"  # provides descriptive statistics about each column in
+    # the DataFrame
 
-    return info_str
+    return data, info_str
