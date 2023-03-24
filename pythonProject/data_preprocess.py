1d2ed0c5800181f7eae4d3189f1e03230232abcd
Kngk0 <k_westbrook@u.pacific.edu>
2023-03-23 18:28:50 -0700

    added preprocessing steps

--- pythonProject/data_preprocess.py
+++ pythonProject/data_preprocess.py
@@ -5,12 +5,26 @@ import csv
 # Import dataLoader function from dataLoader file
 from data_loader import data_loader
 
+
 # Preprocess the data
 def data_preprocess(data):
+    # Data Cleaning - Check to see if the dataset has any missing or "null" values. If there are blanks, you can either
+    #                 get rid of them or fill them in with the right value. Take out any data points that are repeated.
     data = data.drop_duplicates()  # check and remove duplicates
     data = data.dropna()  # remove null values
 
+    # Data Scaling - Make sure that all the features are the same size by putting them on the same scale.
 
+    # Data Encoding - If the dataset has categorical features, like gender or type of disease, you should turn them into
+    #                 numbers that the model can use.
+
+    # Data Features - Choose the most important features for the machine learning model and get rid of any features that
+    #                 are redundant or don't matter.
+
+    # Train-Test Split - Break up the data into sets for training, validating, and testing.
+
+
+'''
 # Convert columns to numeric data types
 Age = df.loc[:, 'Age']
 
@@ -50,4 +64,5 @@ count_lvh = sorted_df['RestingECG'].value_counts()['LVH']
 print('Lowest Cholesterol:', low_value)
 print('Highest Cholesterol:', high_value)
 print('Number of normal:', count_normal)
-print('Number of ST:', count_st)
+print('Number of ST:', count_st')
+'''
