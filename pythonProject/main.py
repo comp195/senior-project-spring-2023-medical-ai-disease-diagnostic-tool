d50ba393cc936158b16e625ca3041302634dac86
mansoorhaidari <100883284+mansoorhaidari@users.noreply.github.com>
2023-03-26 14:31:23 -0700

    Scatter Plot added

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -4,7 +4,7 @@
 # Korie Westbrook
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from data_loader import data_loader
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -57,28 +57,50 @@ with open('heart.csv', 'r') as f:
     # Split lines into fields
     fields = [line.split(';') for line in lines]
 
-# Create a pandas DataFrame from the fields
-df = pd.DataFrame(fields[1:], columns=fields[0])
 
+# Create a pandas DataFrame from the fields
+print(data_loader())
+df =data_loader()
+#print("hello")
+#columns1=fields[0][0]
+#df = pd.DataFrame(fields[1:], columns=fields[0][0].split(','))
+#print(df.keys())
 # Sort the DataFrame by a category
-sorted_df = df.sort_values(by='category')
-print(f'sorted_df')
+#print(df.columns.tolist())
+#df_sorted =df.sort_values(['Age,Sex,ChestPainType,RestingBP,Cholesterol,FastingBS,RestingECG,MaxHR,ExerciseAngina,Oldpeak,ST_Slope,HeartDisease'])
+print(type(df))
+#df= pd.DataFrame(df, columns= ['Age','Sex','ChestPainType','RestingBP','Cholesterol','FastingBS','RestingECG','MaxHR','ExerciseAngina','Oldpeak','ST_Slope','HeartDisease'])
+data, infor_str = data_loader()
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+print(data)
+#df_sorted = df.sort_values(by='category')
+
+#sorted_df = df.sort_values('AGE')
+#print(f'sorted_df')
+
+
+#print(df_sorted)
+
+
 
 # Identify the high and low values for a specified column
 column_name = 'column_name'
-low_value = sorted_df[column_name].min()
-high_value = sorted_df[column_name].max()
+#low_value = sorted_df[column_name].min()
+#high_value = sorted_df[column_name].max()
 
 # Identify the number of 'normal' and 'ST' values in a different column
-count_normal = sorted_df[sorted_df['other_column'] == 'normal'].shape[0]
-count_st = sorted_df[sorted_df['other_column'] == 'ST'].shape[0]
+#count_normal = sorted_df[sorted_df['other_column'] == 'normal'].shape[0]
+#count_st = sorted_df[sorted_df['other_column'] == 'ST'].shape[0]
 
 # Loop through every value in the sorted DataFrame and print it
-for index, row in sorted_df.iterrows():
-    for col in sorted_df.columns:
-        print(row[col])
+#for index, row in sorted_df.iterrows():
+    #for col in sorted_df.columns:
+        #print(row[col])
 
-print('Lowest', column_name, ':', low_value)
-print('Highest', column_name, ':', high_value)
-print('Number of normal:', count_normal)
-print('Number of ST:', count_st)
+#print('Lowest', column_name, ':', low_value)
+#print('Highest', column_name, ':', high_value)
+#print('Number of normal:', count_normal)
+#print('Number of ST:', count_st)
+
+
