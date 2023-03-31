2e5c8b97b088389ba3b3cb01510e33744a3070aa
mansoorhaidari <100883284+mansoorhaidari@users.noreply.github.com>
2023-03-30 18:25:18 -0700

    update to data preprocess

--- pythonProject/data_preprocess.py
+++ pythonProject/data_preprocess.py
@@ -183,6 +183,21 @@ def data_preprocess(data_load):
     plt.ylabel('Number of Patients')
     plt.show()
 
+    # create a figure with subplots for each column
+    fig, axs = plt.subplots(nrows=3, ncols=4, figsize=(20, 15))
+
+    # loop over each column and plot a histogram
+    for i, col in enumerate(df.columns):
+        ax = axs[i // 4, i % 4]  # select the appropriate subplot
+        ax.hist(df[col], bins=20, color='blue', alpha=0.5)  # plot histogram
+        ax.set_title(col)  # set the subplot title
+
+    # adjust the spacing between subplots
+    plt.tight_layout()
+
+    # show the plot
+    plt.show()
+
     #       Data Scaling - Make sure that all the features are the same size by putting them on the same scale.
     #                      to make sure that high-value features don't take over the model and skew the results.
 
