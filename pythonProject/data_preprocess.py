75c2a40473a94a3a3c1452d3b12e194fd1117a6d
mansoorhaidari <100883284+mansoorhaidari@users.noreply.github.com>
2023-04-09 16:01:54 -0700

    added Chai-Square data correlation, and data recursive feature

--- pythonProject/data_preprocess.py
+++ pythonProject/data_preprocess.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylab as pl
 from pyexpat import model
+
+from scipy.stats import chi2_contingency
+from sklearn.feature_selection import RFE
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from data_loader import data_loader
@@ -292,6 +295,7 @@ def data_preprocess(file):
 
 
 
+
     #       Handle Outliers - Outliers may be removed or adjusted. Outliers may be removed if they are data input
     #                         mistakes or affect model performance. You may also replace outliers with more realistic
     #                         numbers. Capping, where extreme values are replaced with the maximum or lowest value
@@ -323,6 +327,174 @@ def data_preprocess(file):
     # Data Features -Choose the dataset's most important features that can be used to make predictions. To find the
     #                most important features, you can use methods like correlation analysis, the chi-squared test, or
     #                recursive feature elimination.
+
+    print('Chi-Squared Test >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+
+    # Load the Heart Disease dataset
+    df = pd.read_csv('heart.csv')
+    # Select the categorical or columns features
+    columns = ['Age', 'Sex', 'ChestPainType', 'RestingBP', 'Cholesterol', 'FastingBS',
+               'RestingECG', 'MaxHR', 'ExerciseAngina', 'Oldpeak', 'ST_Slope', 'HeartDisease']
+    # Loop through all pairs of categorical features and calculate the chi-square statistic and p-value
+    for i in range(len(columns)):
+        for j in range(i + 1, len(columns)):
+            contingency_table = pd.crosstab(df[columns[i]], df[columns[j]])
+            chi2, p, dof, expected = chi2_contingency(contingency_table)
+            print(f'Chi-square test between {columns[i]} and {columns[j]}:')
+            print(f'    Chi-square statistic = {chi2:.2f}')
+            print(f'    p-value = {p:.5f}')
+            print(f'    Degrees of freedom = {dof}')
+
+    print('Correlation Analysis >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+
+    # load the heart disease dataset
+    df = pd.read_csv('heart.csv')
+    # calculate the correlation matrix
+    corr_matrix = df.corr()
+    # display the correlation matrix
+    print(corr_matrix)
+    # select the most highly correlated features
+    most_correlated = corr_matrix.nlargest(10, 'HeartDisease')['HeartDisease'].index
+    # display the most highly correlated features
+    print(most_correlated)
+
+    # Create sample dataframe
+    data = {'Age': [1.000000, 0.254078, -0.236358, 0.211500, -0.404093, 0.266694, 0.319918],
+            'RestingBP': [0.254078, 1.000000, 0.102408, 0.021684, -0.126433, 0.142644, 0.052769],
+            'Cholesterol': [-0.236358, 0.102408, 1.000000, 0.143055, -0.009940, 0.012608, -0.317407],
+            'FastingBS': [0.211500, 0.021684, 0.143055, 1.000000, 0.046886, 0.100919, 0.353210],
+            'MaxHR': [-0.404093, -0.126433, -0.009940, 0.046886, 1.000000, -0.122922, -0.372111],
+            'Oldpeak': [0.266694, 0.142644, 0.012608, 0.100919, -0.122922, 1.000000, 0.421706],
+            'HeartDisease': [0.319918, 0.052769, -0.317407, 0.353210, -0.372111, 0.421706, 1.000000]}
+
+    df = pd.DataFrame(data)
+
+    # Get column names of dataframe
+    cols = df.columns
+
+    # Create figure with subplots
+    fig, axes = plt.subplots(nrows=len(cols), ncols=len(cols), figsize=(10, 10))
+
+    # Loop through all variables and create scatter plot with best fit line
+    for i, var1 in enumerate(cols):
+        for j, var2 in enumerate(cols):
+            if i == j:
+                # Create empty plot if variables are the same
+                axes[i, j].axis('off')
+            else:
+                # Create scatter plot with color based on correlation
+                color = 'r' if df[var1].corr(df[var2]) > 0 else 'b'
+                sns.regplot(ax=axes[i, j], x=var1, y=var2, data=df, color=color)
+                # Add best fit line to scatter plot
+                sns.regplot(ax=axes[i, j], x=var1, y=var2, data=df, line_kws={'color': 'k'}, scatter=False)
+                # Add title to scatter plot
+                axes[i, j].set_title(f"{var1} vs {var2}")
+                # Set x and y axis labels for scatter plot
+                axes[i, j].set_xlabel(var1)
+                axes[i, j].set_ylabel(var2)
+
+    plt.suptitle("Scatter Plots with Best Fit Line for Correlation Matrix", fontsize=16)
+    plt.tight_layout()
+    plt.show()
+
+    print('Recursive Feature Elimination >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+
+    # Recursive Feature Elimination:
+
+    # Load the dataset
+    df = pd.read_csv('heart.csv')
+
+    # Encode the categorical features using one-hot encoding
+    df = pd.get_dummies(df, columns=['Sex', 'ChestPainType', 'FastingBS', 'RestingECG', 'ExerciseAngina', 'ST_Slope'])
+
+    # Separate the target variable from the features
+    X = df.drop('HeartDisease', axis=1)
+    y = df['HeartDisease']
+
+    # Create the RFE object and fit it to the data
+    estimator = LogisticRegression(solver='lbfgs', max_iter=1000)
+    rfe = RFE(estimator, n_features_to_select=6)
+    rfe.fit(X, y)
+
+    # Print the selected features
+    print(X.columns[rfe.support_])
+
+    print('Bar Chart>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+
+    # Define a dictionary to store the associations and their p-values
+    associations = {
+        "Age and Sex": 0.11735,
+        "Age and ChestPainType": 0.00002,
+        "Age and RestingBP": 0.95938,
+        "Age and Cholesterol": 0.04217,
+        "Age and FastingBS": 0.13338,
+        "Age and RestingECG": 0.03654,
+        "Age and MaxHR": 0.01928,
+        "Age and ExerciseAngina": 0.00138,
+        "Age and Oldpeak": 0.99969,
+        "Age and ST_Slope": 0.01690,
+        "Age and HeartDisease": 0.00001,
+        "Sex and ChestPainType": 0.00000,
+        "Sex and RestingBP": 0.78306,
+        "Sex and Cholesterol": 0.06093,
+        "Sex and FastingBS": 0.00168,
+        "Sex and RestingECG": 0.49360,
+        "Sex and MaxHR": 0.83364,
+        "Sex and ExerciseAngina": 0.00005,
+        "Sex and Oldpeak": 0.71862,
+        "Sex and ST_Slope": 0.00030,
+        "Sex and HeartDisease": 0.00000,
+        "ChestPainType and RestingBP": 0.85288,
+        "ChestPainType and Cholesterol": 0.00178,
+        "ChestPainType and FastingBS": 0.00000,
+        "ChestPainType and RestingECG": 0.08041,
+        "ChestPainType and MaxHR": 0.08228,
+        "ChestPainType and ExerciseAngina": 0.00000,
+        "ChestPainType and Oldpeak": 0.00014,
+        "ChestPainType and ST_Slope": 0.00000,
+        "ChestPainType and HeartDisease": 0.00000
+    }
+
+    # Sort the associations based on their p-values
+    sorted_associations = sorted(associations.items(), key=lambda x: x[1])
+
+    # Extract the association names and p-values into separate lists
+    associations_list = [x[0] for x in sorted_associations]
+    p_values_list = [x[1] for x in sorted_associations]
+
+    # Create a bar chart of the associations and their p-values
+    plt.figure(figsize=(12, 8))
+    plt.barh(associations_list, p_values_list, color='purple')
+    plt.xlabel('p-value')
+    plt.title('Associations and their p-values')
+    plt.tight_layout()
+
+    # Show the chart
+    plt.show()
+
+    print('Pie Chart>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    # Define the data
+    male_heart_disease = 20
+    male_no_heart_disease = 80
+    female_heart_disease = 10
+    female_no_heart_disease = 90
+
+    # Define the labels and colors
+    labels = ['Male Heart Disease', 'Male No Heart Disease', 'Female Heart Disease', 'Female No Heart Disease']
+    colors = ['red', 'orange', 'pink', 'lightcoral']
+
+    # Define the sizes
+    sizes = [male_heart_disease, male_no_heart_disease, female_heart_disease, female_no_heart_disease]
+
+    # Create the pie chart
+    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+
+    # Add a title
+    plt.title('Heart Disease Cases by Gender')
+
+    # Show the chart
+    plt.show()
+
 
     return data_load
 
