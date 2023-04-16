35682ce3d38a19d9885834783dcc9f457e064065
mansoorhaidari <100883284+mansoorhaidari@users.noreply.github.com>
2023-04-16 14:18:58 -0700

    N/A

--- pythonProject/tkintertest.py
+++ pythonProject/tkintertest.py
@@ -1,66 +1,181 @@
-import tkinter as tk
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from tkinter import *
+import os
 
 
-class MyApp:
-    def __init__(self, master):
-        self.master = master
-        master.title("My App")
-        self.master.geometry('600x500')
-
-        self.label = tk.Label(master, text="Select Feature Set:")
-        self.label.pack()
-
-        self.feature_set = tk.StringVar(value='all_features')
-        self.feature_set_options = ['all_features', 'corr_features', 'chi_features', 'rfe_features']
-        self.feature_set_menu = tk.OptionMenu(master, self.feature_set, *self.feature_set_options)
-        self.feature_set_menu.pack()
-
-        self.button = tk.Button(master, text="Get Accuracy", command=self.display_accuracy)
-        self.button.pack()
-
-        self.figure_canvas = None
-
-    def display_accuracy(self):
-        processed_data = pd.read_csv('processed_data.csv')
-        corr_features = pd.read_csv('corr_features.csv', header=None).iloc[:, 0].tolist()
-        chi2_features = pd.read_csv('chi2_features.csv', header=None).iloc[:, 0].tolist()
-        rfe_features = pd.read_csv('rfe_features.csv', header=None).iloc[:, 0].tolist()
-        feature_sets = {
-            'all_features': processed_data.drop('HeartDisease', axis=1).columns.tolist(),
-            'corr_features': corr_features,
-            'chi_features': chi2_features,
-            'rfe_features': rfe_features,
-        }
-
-        features = feature_sets[self.feature_set.get()]
-        X = processed_data[features]
-        y = processed_data['HeartDisease']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        model = LogisticRegression()
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-
-        if self.figure_canvas:
-            self.figure_canvas.get_tk_widget().pack_forget()
-
-        fig, ax = plt.subplots(figsize=(4, 4))
-        ax.bar(['Accuracy'], [accuracy])
-        ax.set_ylim([0, 1])
-        ax.set_title(f'Accuracy for {self.feature_set.get()}')
-        ax.set_ylabel('Accuracy')
-        self.figure_canvas = FigureCanvasTkAgg(fig, master=self.master)
-        self.figure_canvas.get_tk_widget().pack()
+# Designing window for registration
+def destroyPackWidget(parent):
+    for e in parent.pack_slaves():
+        e.destroy()
 
 
+def register():
+    global root, register_screen
 
-if __name__ == '__main__':
-    root = tk.Tk()
-    myapp = MyApp(root)
-    root.mainloop()
+    # Adding a comment
+    destroyPackWidget(root)
+    register_screen = root
+    #    register_screen = Toplevel(main_screen)
+    register_screen.title("Register")
+    register_screen.geometry("300x250")
+
+    global username
+    global password
+    global username_entry
+    global password_entry
+    username = StringVar()
+    password = StringVar()
+
+    Label(register_screen, text="Please enter details below", bg="blue").pack()
+    Label(register_screen, text="").pack()
+    username_lable = Label(register_screen, text="Username * ")
+    username_lable.pack()
+    username_entry = Entry(register_screen, textvariable=username)
+    username_entry.pack()
+    password_lable = Label(register_screen, text="Password * ")
+    password_lable.pack()
+    password_entry = Entry(register_screen, textvariable=password, show='*')
+    password_entry.pack()
+    Label(register_screen, text="").pack()
+    Button(register_screen, text="Register", width=10, height=1, bg="blue", command=register_user).pack()
+
+
+# Designing window for login
+
+def login(main_screen=None):
+    global login_screen
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Login")
+    login_screen.geometry("300x250")
+    Label(login_screen, text="Please enter details below to login").pack()
+    Label(login_screen, text="").pack()
+
+    global username_verify
+    global password_verify
+
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+    global username_login_entry
+    global password_login_entry
+
+    Label(login_screen, text="Username * ").pack()
+    username_login_entry = Entry(login_screen, textvariable=username_verify)
+    username_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Label(login_screen, text="Password * ").pack()
+    password_login_entry = Entry(login_screen, textvariable=password_verify, show='*')
+    password_login_entry.pack()
+    Label(login_screen, text="").pack()
+    Button(login_screen, text="Login", width=10, height=1, command=login_verify).pack()
+
+
+# Implementing event on register button
+def btnSucess_Click():
+    global root
+    destroyPackWidget(root)
+
+
+def register_user():
+    global root, username, password
+    username_info = username.get()
+    password_info = password.get()
+    print("abc", username_info, password_info, "xyz")
+    file = open(username_info, "w")
+    file.write(username_info + "\n")
+    file.write(password_info)
+    file.close()
+
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+    Label(root, text="Registration Success", fg="green", font=("calibri", 11)).pack()
+    Button(root, text="Click Here to proceed", command=btnSucess_Click).pack()
+
+
+# Implementing event on login button
+
+def login_verify():
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_login_entry.delete(0, END)
+    password_login_entry.delete(0, END)
+
+    list_of_files = os.listdir()
+    if username1 in list_of_files:
+        file1 = open(username1, "r")
+        verify = file1.read().splitlines()
+        if password1 in verify:
+            login_sucess()
+
+        else:
+            password_not_recognised()
+
+    else:
+        user_not_found()
+
+
+# Designing popup for login success
+
+def login_sucess():
+    global login_success_screen
+    login_success_screen = Toplevel(login_screen)
+    login_success_screen.title("Success")
+    login_success_screen.geometry("150x100")
+    Label(login_success_screen, text="Login Success").pack()
+    Button(login_success_screen, text="OK", command=delete_login_success).pack()
+
+
+# Designing popup for login invalid password
+
+def password_not_recognised():
+    global password_not_recog_screen
+    password_not_recog_screen = Toplevel(login_screen)
+    password_not_recog_screen.title("Success")
+    password_not_recog_screen.geometry("150x100")
+    Label(password_not_recog_screen, text="Invalid Password ").pack()
+    Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
+
+
+# Designing popup for user not found
+
+def user_not_found():
+    global user_not_found_screen
+    user_not_found_screen = Toplevel(login_screen)
+    user_not_found_screen.title("Success")
+    user_not_found_screen.geometry("150x100")
+    Label(user_not_found_screen, text="User Not Found").pack()
+    Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
+
+
+# Deleting popups
+
+def delete_login_success():
+    login_success_screen.destroy()
+
+
+def delete_password_not_recognised():
+    password_not_recog_screen.destroy()
+
+
+def delete_user_not_found_screen():
+    user_not_found_screen.destroy()
+
+
+# Designing Main(first) window
+
+def main_account_screen(frmmain):
+    main_screen = frmmain
+
+    main_screen.geometry("300x250")
+    main_screen.title("Account Login")
+    Label(main_screen, text="Select Your Choice", bg="blue", width="300", height="2", font=("Calibri", 13)).pack()
+    Label(main_screen, text="").pack()
+    Button(main_screen, text="Login", height="2", width="30", command=login).pack()
+    Label(main_screen, text="").pack()
+    Button(main_screen, text="Register", height="2", width="30", command=register).pack()
+
+
+root = Tk()
+main_account_screen(root)
+
+root.mainloop()
